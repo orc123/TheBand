2138e0425c028a433bb1e4ee078c06c028c84fe8
Dong Pham <dong.orc12@gmail.com>
2023-04-28 00:12:07 +0700

    responsive mobile header

---
+++ assets/js/header.js
@@ -0,0 +1,34 @@
+﻿var headerControler = {
+    init: function () {
+        this.loadMenuForMobile();
+        this.selectMenuEvent();
+    },
+
+    // Đóng/ mở menu
+    loadMenuForMobile: function () {
+        var header = document.getElementById('header');
+        var mobileMenu = document.getElementById('mobile-menu');
+        var headerHeight = header.clientHeight
+        mobileMenu.onclick = function () {
+            if (header.clientHeight === headerHeight) {
+                header.style.height = 'auto';
+            }
+            else {
+                header.style.height = null;
+            }
+        }
+    },
+
+    // Tự động đóng khi chọn menu
+    selectMenuEvent() {
+        var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+        var header = document.getElementById('header');
+        for (var i = 0; i < menuItems.length; i++) {
+            menuItems[i].onclick = function () {
+                header.style.height = null;
+            }
+        }
+    }
+}
+
+headerControler.init();
